f8c44b0f70f410f074f75a316a53cfc0469e47e5
Felipe Zingoni <felipezingoni.fz@gmail.com>
2022-05-17 14:40:25 -0300

    heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,12 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "chartkick/chart.js"
-import "Chart.bundle"
+// import "Chart.bundle"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
 
-import "controllers"
+// import "controllers"
 import "bootstrap"
