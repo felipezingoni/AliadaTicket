a25ad6940a8e9f3a06329d0293d3a41001cc39b1
Felipe Zingoni <felipezingoni.fz@gmail.com>
2022-05-17 12:50:30 -0300

    changes

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module NAMEPROJECT
     config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
-
+    config.assets.initialize_on_precompile = false
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
